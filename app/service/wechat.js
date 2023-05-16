613d17b0c7f575318c789d62bc211192705cee1a
虞玲红 <3247891181@qq.com>
2023-05-16 19:36:04 +0800

    微信绑定用户修改

--- app/service/wechat.js
+++ app/service/wechat.js
@@ -8,22 +8,32 @@ class WxMiniProgramService extends Service {
   }
   async bind(payload) {
     const { ctx } = this;
-    return await ctx.model.UserWechatInfo.upsert(payload);
+    const { unionid } = payload;
+    const UserWechatInfo = ctx.model.UserWechatInfo;
+    const user_wechat_info = await UserWechatInfo.findOne({ where: { unionid } });
+    if (user_wechat_info) {
+      ctx.logger.error('用户微信绑定bind==================', user_wechat_info);
+      return await UserWechatInfo.update(payload, { where: { unionid } });
+    }
+    ctx.logger.error('微信用户绑定bind，没有绑定数据，新建==================');
+    return await UserWechatInfo.create(payload);
   }
   // 公众号取消关注
   async gh_unsubscribe(gh_openid) {
     const { ctx } = this;
-    const user_wechat_info = await ctx.model.UserWechatInfo.findOne({
+    const UserWechatInfo = ctx.model.UserWechatInfo;
+    const user_wechat_info = await UserWechatInfo.findOne({
       where: { gh_openid },
+      raw: true,
     });
     if (user_wechat_info) {
       if (user_wechat_info.mini_openid) {
         // 更新
-        await user_wechat_info.update({
+        await UserWechatInfo.update({
           gh_openid: '',
-        });
+        }, { where: { gh_openid } });
       } else {
-        user_wechat_info.destroy();
+        UserWechatInfo.destroy({ where: { gh_openid } });
       }
     }
   }
